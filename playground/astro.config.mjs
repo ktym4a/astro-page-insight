83a753305e034f780b7fd3992a3de9474a578dd7
ktym4a <shoma@ktym4a.me>
2024-03-09 20:55:29 +0700

    Add PWA support to Astro project

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,4 +1,5 @@
 import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
 import astroPageInsight from "astro-page-insight";
 import { defineConfig } from "astro/config";
 
@@ -12,6 +13,32 @@ export default defineConfig({
 			},
 			// firstFetch: "open",
 			experimentalCache: true,
+		}),
+		AstroPWA({
+			mode: "development",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Astro PWA",
+				short_name: "Astro PWA",
+				theme_color: "#ffffff",
+			},
+			pwaAssets: {
+				config: true,
+			},
+			workbox: {
+				navigateFallback: "/",
+				globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+			},
+			devOptions: {
+				enabled: true,
+				navigateFallbackAllowlist: [/^\//],
+			},
+			experimental: {
+				directoryAndTrailingSlashHandler: true,
+			},
 		}),
 	],
 });
