844be759e8cce1e42ce41f096ce315a3a4ff9ea4
ktym4a <shoma@ktym4a.me>
2024-03-10 14:43:31 +0700

    add test data

--- playground/src/env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/info" />
-/// <reference types="vite-plugin-pwa/vanillajs" />
---
+++ playground/ssr/astro.config.mjs
@@ -0,0 +1,22 @@
+import node from "@astrojs/node";
+import svelte from "@astrojs/svelte";
+import astroPageInsight from "astro-page-insight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	integrations: [
+		svelte(),
+		astroPageInsight({
+			lh: {
+				pwa: true,
+			},
+			// firstFetch: "open",
+			experimentalCache: true,
+		}),
+	],
+});
