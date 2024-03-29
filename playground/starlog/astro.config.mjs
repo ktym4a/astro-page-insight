5a66515a2412c8d9532d49ecefb0797cd05b0169
ktym4a <shoma@ktym4a.me>
2024-03-29 11:35:38 +0700

    add `starlog` playground and remove `view-transitions` playground (#68)

---
+++ playground/starlog/astro.config.mjs
@@ -0,0 +1,16 @@
+import astroPageInsight from "astro-page-insight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://example.com",
+	integrations: [
+		astroPageInsight({
+			lh: {
+				pwa: true,
+			},
+			// firstFetch: "open",
+			experimentalCache: true,
+		}),
+	],
+});
