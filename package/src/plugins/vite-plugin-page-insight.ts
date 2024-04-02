9f06003078f4beddcc92ec9eee16169e7ec16465
ktym4a <shoma@ktym4a.me>
2024-04-02 22:18:23 +0700

    Add logger to astroScriptsPlugin

--- package/src/plugins/vite-plugin-page-insight.ts
+++ package/src/plugins/vite-plugin-page-insight.ts
@@ -1,7 +1,11 @@
 import fs from "node:fs";
+import type { AstroIntegrationLogger } from "astro";
 import { type ConfigEnv, type Plugin as VitePlugin, normalizePath } from "vite";
 
-export const astroScriptsPlugin = (cacheDir: string): VitePlugin => {
+export const astroScriptsPlugin = (
+	cacheDir: string,
+	logger: AstroIntegrationLogger,
+): VitePlugin => {
 	let env: ConfigEnv | undefined = undefined;
 	return {
 		name: "vite-plugin-page-insight",
@@ -13,25 +17,29 @@ export const astroScriptsPlugin = (cacheDir: string): VitePlugin => {
 			const isSsrBuild = env?.isSsrBuild;
 			if (env?.command === "build" && !isSsrBuild) {
 				const normalizeCachePath = normalizePath(cacheDir);
-				const files = fs
-					.readdirSync(normalizeCachePath, {
-						recursive: true,
-						withFileTypes: true,
-					})
-					.filter((dirent) => dirent.isFile())
-					.map((dirent) => `${dirent.path}/${dirent.name}`);
+				try {
+					const files = fs
+						.readdirSync(normalizeCachePath, {
+							recursive: true,
+							withFileTypes: true,
+						})
+						.filter((dirent) => dirent.isFile())
+						.map((dirent) => `${dirent.path}/${dirent.name}`);
 
-				for (const filePath of files) {
-					const normalizeFilePath = normalizePath(filePath);
-					if (!normalizeFilePath.endsWith(".json")) {
-						continue;
+					for (const filePath of files) {
+						const normalizeFilePath = normalizePath(filePath);
+						if (!normalizeFilePath.endsWith(".json")) {
+							continue;
+						}
+						const content = fs.readFileSync(normalizeFilePath, "utf-8");
+						this.emitFile({
+							type: "asset",
+							fileName: normalizeFilePath,
+							source: JSON.stringify(JSON.parse(content)),
+						});
 					}
-					const content = fs.readFileSync(normalizeFilePath, "utf-8");
-					this.emitFile({
-						type: "asset",
-						fileName: normalizeFilePath,
-						source: JSON.stringify(JSON.parse(content)),
-					});
+				} catch (error) {
+					logger.info("No cache files found.");
 				}
 			}
 		},
